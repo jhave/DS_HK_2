af52bcca8f88435a220b844a5ea81d3caf215942
jhave <jhave2@gmail.com>
2014-07-31 22:19:19 +0800

    import_utilities includes SYNSET replacement, and sundry functions (just uploading as i am about to mutate it prior to using ohhla rap_mouth reservoir)

--- code/student/ongoing/poetry-jhave/import_utilities/__init__.py
+++ code/student/ongoing/poetry-jhave/import_utilities/__init__.py
@@ -38,6 +38,11 @@ from pattern.en import conjugate, lemma, lexeme, PARTICIPLE
 # from pattern.en import sentiment
 # and can be used with weights using http://sentiwordnet.isti.cnr.it/ 
 
+##############################################################
+# stoplist http://www.lextek.com/manuals/onix/stopwords1.html
+# more comprehensive than default in nltk
+
+stopwords_ls=["a","about","above","across","after","again","against","all","almost","alone","along","already","also","although","always","among","an","and","another","any","anybody","anyone","anything","anywhere","are","area","areas","around","as","ask","asked","asking","asks","at","away","b","back","backed","backing","backs","be","became","because","become","becomes","been","before","began","behind","being","beings","best","better","between","big","both","but","by","c","came","can","cannot","case","cases","certain","certainly","clear","clearly","come","could","d","did","differ","different","differently","do","does","done","down","downed","downing","downs","during","e","each","early","either","end","ended","ending","ends","enough","even","evenly","ever","every","everybody","everyone","everything","everywhere","f","face","faces","fact","facts","far","felt","few","find","finds","first","for","four","from","full","fully","further","furthered","furthering","furthers","g","gave","general","generally","get","gets","give","given","gives","go","going","good","goods","got","great","greater","greatest","group","grouped","grouping","groups","h","had","has","have","having","he","her","here","herself","high","higher","highest","him","himself","his","how","however","i","if","important","in","interest","interested","interesting","interests","into","is","it","its","itself","j","just","k","keep","keeps","kind","knew","know","known","knows","l","large","largely","last","later","latest","least","less","let","lets","like","likely","long","longer","longest","m","made","make","making","man","many","may","me","member","members","men","might","more","most","mostly","mr","mrs","much","must","my","myself","n","necessary","need","needed","needing","needs","never","new","newer","newest","next","no","nobody","non","noone","not","nothing","now","nowhere","number","numbers","o","of","off","often","old","older","oldest","on","once","one","only","open","opened","opening","opens","or","order","ordered","ordering","orders","other","others","our","out","over","p","part","parted","parting","parts","per","perhaps","place","places","point","pointed","pointing","points","possible","present","presented","presenting","presents","problem","problems","put","puts","q","quite","r","rather","really","right","room","rooms","s","said","same","saw","say","says","second","seconds","see","seem","seemed","seeming","seems","sees","several","shall","she","should","show","showed","showing","shows","side","sides","since","small","smaller","smallest","so","some","somebody","someone","something","somewhere","state","states","still","such","sure","t","take","taken","than","that","the","their","them","then","there","therefore","these","they","thing","things","think","thinks","this","those","though","thought","thoughts","three","through","thus","to","today","together","too","took","toward","turn","turned","turning","turns","two","u","under","until","up","upon","us","use","used","uses","v","very","w","want","wanted","wanting","wants","was","way","ways","we","well","wells","went","were","what","when","where","whether","which","while","who","whole","whose","why","will","with","within","without","work","worked","working","works","would","x","y","year","years","yet","you","young","younger","youngest","your","yours","z"]
 
 personal_pronouns = ["i","me","we","us","you","she","her","he","him","it","they","them"]
 
@@ -304,7 +309,8 @@ chars = {
     '\xcc\xa8' : '',         # modifier - under curve
     '\xcc\xb1' : '' ,        # modifier - under line
     '\xe2\x80\x99': '\'',   # apostrophe
-    '\xe2\x80\x94': '--'    # em dash
+    '\xe2\x80\x94': '--',    # em dash \xe2\x80\x94
+    '\xe2':"a"
 
 }
 
@@ -313,6 +319,14 @@ chars = {
 def replace_chars(match):
     char = match.group(0)
     return chars[char]
+
+# USAGE new_str = re.sub('(' + '|'.join(chars.keys()) + ')', replace_chars, text)
+def isGarbageInWord_bool(word):
+    for l in word.split():
+        for ch in chars:
+            if l==ch:
+                return True
+    return False
 
 
 heres=["nowhere","somewhere","there","anywhere","not here","here"]
